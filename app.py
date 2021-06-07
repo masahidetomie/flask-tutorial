ae066d45bbf51fa65e03e5126736637adc56abd8
masahidetomie <toda2020-009@iniad.org>
2021-06-07 22:13:14 +0000

    uranai adding

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask,render_template
+import random
 app= Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    unsei_list=["大吉","中吉","小吉"]
+    uranai=random.choice(unsei_list)
+    return render_template('index.html',fortune=uranai,fortune_list=unsei_list)
 
 if __name__=="__main__":
     app.run(debug=True)
